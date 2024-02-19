b27b2686e54aceeb654f06c9e8b1356357b00507
nox <samingosi97@gmail.com>
2024-02-18 23:14:18 -0800

    implemented bubbe sort with size constrictions

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,19 @@
 #include "sort.h"
 /**
+ * swap - function to swap two integers
+ * @a: first int
+ * @b: second int
+ * Return: no return, void
+ */
+void swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+/**
  * bubble_sort - function that implements th bubble sort sorting algorithm
  * @array: Array to be sorted
  * @size: size of the array
@@ -8,18 +22,17 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t swapped = 1, i;
-	int temp;
 
+	if (size < 2)
+		return;
 	while (swapped)
 	{
+		swapped = 0;
 		for (i = 0; i < (size - 1); i++)
 		{
-			swapped = 0;
-			if (array[i] < array[i + 1])
+			if (array[i] > array[i + 1])
 			{
-				temp = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = temp;
+				swap(&array[i], &array[i + 1]);
 				swapped = 1;
 				print_array((const int *)array, size);
 			}
