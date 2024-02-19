3cfca478a91196c55c765a292a21b799571b74c1
nox <samingosi97@gmail.com>
2024-02-18 23:21:16 -0800

    add print array and print lis

--- sort.h
+++ sort.h
@@ -31,7 +31,7 @@ typedef enum kind_e
 	SPADE = 0,
 	HEART,
 	CLUB,
-    DIAMOND
+	DIAMOND
 } kind_t;
 
 /**
@@ -43,8 +43,8 @@ typedef enum kind_e
  */
 typedef struct card_s
 {
-    const char *value;
-    const kind_t kind;
+	const char *value;
+	const kind_t kind;
 } card_t;
 
 /**
@@ -56,9 +56,9 @@ typedef struct card_s
  */
 typedef struct deck_node_s
 {
-    const card_t *card;
-    struct deck_node_s *prev;
-    struct deck_node_s *next;
+	const card_t *card;
+	struct deck_node_s *prev;
+	struct deck_node_s *next;
 } deck_node_t;
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
