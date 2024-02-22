e8691d5a07759d9e6fb7445d094a4bb74818857e
nox <samingosi97@gmail.com>
2024-02-22 06:03:02 -0800

    look for a comp

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -19,44 +19,14 @@ void swap(int *one, int *two)
  * @size: size of the array to sort // last element
  * Return: index where the partitin ended
  */
-int sort_part(int *a, int size)
+int sort_part(int *a, int left, int right)
 {
 	int pivot, i, j;
 
-	pivot = a[0];
-	i = 0;
-	j = size - 1;
-	while (i < j)
-	{
-		do
-		{
-			i++;
-		}while (a[i] <= pivot);
-		do
-		{
-			j--;
-		}while (a[j] > pivot);
-		if (i < j)
-			swap(&a[i], &a[j]);
-	}
-	swap(&a[0], &a[j]);
-	return j;
-}
-/**
- * @array: array of ints to sort
- * @size: size of the array
- * Return: void since it usess pointers to sort in place
- */
-void quick_sort(int *array, size_t size)
-{
-	int j;
+	pivot = a[left];
+	i = left + 1;
+	j = right;
 
-	if (size < 2)
-		return;
-	if (size > 1)
+	while (1)
 	{
-		j = sort_part(array, size - 1);
-		quick_sort(array, j);
-		quick_sort(&array[j + 1], size);
-	}
-}
+		while ( i <= j &&
